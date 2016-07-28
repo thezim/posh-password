367ab65df10fae6367df0295be06ebf55daa056f
Greg Zimmerman <thezim@gmail.com>
2016-07-28 12:13:38 -0400

    Made < PS4 compatible,

--- posh-password.psm1
+++ posh-password.psm1
@@ -11,16 +11,17 @@
         Write-Error -Message "Length too short. Minimum character length is $minlength."
         return
     }
-    $lowers = (97..122).ForEach{[char]$_}
-    $uppers = (65..90).ForEach{[char]$_}
-    $symbols = (33..47).ForEach{[char]$_}+(58..64).ForEach{[char]$_}
-    $numbers = (48..57).ForEach{[char]$_}
+    $lowers = (97..122) | ForEach-Object {[char]$_}
+    $uppers = (65..90) | ForEach-Object {[char]$_}
+    $symbols = ((33..47) | ForEach-Object {[char]$_})
+    $symbols += ((58..64) | ForEach-Object {[char]$_})
+    $numbers = (48..57) | ForEach-Object {[char]$_}
     $all = $lowers + $uppers + $symbols + $numbers
     $pwd = @()
-    if($UpperLetter -ne 0){(Get-Random -Count $UpperLetter -InputObject $uppers).ForEach({$pwd+=$_})}
-    if($LowerLetter -ne 0){(Get-Random -Count $LowerLetter -InputObject $lowers).ForEach({$pwd+=$_})}
-    if($Symbol -ne 0){(Get-Random -Count $Symbol -InputObject $symbols).ForEach({$pwd+=$_})}
-    if($Number -ne 0){(Get-Random -Count $Number -InputObject $numbers).ForEach({$pwd+=$_})}
+    if($UpperLetter -ne 0){(Get-Random -Count $UpperLetter -InputObject $uppers) | ForEach-Object {$pwd+=$_}}
+    if($LowerLetter -ne 0){(Get-Random -Count $LowerLetter -InputObject $lowers) | ForEach-Object {$pwd+=$_}}
+    if($Symbol -ne 0){(Get-Random -Count $Symbol -InputObject $symbols) | ForEach-Object {$pwd+=$_}}
+    if($Number -ne 0){(Get-Random -Count $Number -InputObject $numbers) | ForEach-Object {$pwd+=$_}}
     if($pwd.Count -lt $Length){
         $pwd += (Get-Random -Count ($Length - $pwd.Count) -InputObject $all)
     }
